eec1f707b97c53a87747a9ef054ab6a680d3e9c7
afridshaikh <afrid75@gmail.com>
2017-07-01 13:26:12 +0530

    many changes

---
+++ js/function.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+// ---------------------------------------- index --------------------------------------
+
+//syllabus
+  $('#be').click(function(){
+    $('#option-be').toggle(300);
+  });
+  $('#te').click(function(){
+    $('#option-te').toggle(300);
+  });
+  $('#se').click(function(){
+    $('#option-se').toggle(300);
+  });
+
+});
