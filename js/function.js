f7dd61b86063e3a280a088314ce362982535e264
afridshaikh <afrid75@gmail.com>
2017-07-03 23:26:54 +0530

    nav and footer shorthanded

--- js/function.js
+++ js/function.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+  $('#nav').load("/nav.html");
+  $('#footer').load("/footer.html");
+  $('#nav').load("../nav.html");
+  $('#footer').load("../footer.html");
 
 // ---------------------------------------- index --------------------------------------
 
