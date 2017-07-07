df78029de89dacaa02d66f637487d8dbb9513212
afridshaikh <afrid75@gmail.com>
2017-07-07 17:49:58 +0530

    img slider, gallary added

--- js/function.js
+++ js/function.js
@@ -39,4 +39,95 @@ $(document).ready(function(){
     $('#option-se').toggle(300);
   });
 
+
+
+// -------------------- faculties
+$('#show-hide1').toggle();
+var angle1 = 180;
+$('#view-more-btn1').click(function() {
+
+  $(this).css('transform', 'rotate('+ angle1 +'deg)');
+  $('#show-hide1').toggle(300);
+  angle1 = 180 - angle1 ;
+});
+
+
+$('#show-hide2').toggle();
+var angle2 = 180;
+$('#view-more-btn2').click(function() {
+
+  $(this).css('transform', 'rotate('+ angle2 +'deg)');
+  $('#show-hide2').toggle(300);
+  angle2 = 180 - angle2 ;
+});
+
+
+$('#show-hide3').toggle();
+var angle3 = 180;
+$('#view-more-btn3').click(function() {
+
+  $(this).css('transform', 'rotate('+ angle3 +'deg)');
+  $('#show-hide3').toggle(300);
+  angle3 = 180 - angle3 ;
+});
+
+
+$('#show-hide4').toggle();
+var angle4 = 180;
+$('#view-more-btn4').click(function() {
+
+  $(this).css('transform', 'rotate('+ angle4 +'deg)');
+  $('#show-hide4').toggle(300);
+  angle4 = 180 - angle4 ;
+});
+
+
+
+$('#show-hide5').toggle();
+var angle5 = 180;
+$('#view-more-btn5').click(function() {
+
+  $(this).css('transform', 'rotate('+ angle5 +'deg)');
+  $('#show-hide5').toggle(300);
+  angle5 = 180 - angle5 ;
+});
+
+
+
+$('#show-hide6').toggle();
+var angle6 = 180;
+$('#view-more-btn6').click(function() {
+
+  $(this).css('transform', 'rotate('+ angle6 +'deg)');
+  $('#show-hide6').toggle(300);
+  angle6 = 180 - angle6 ;
+});
+
+
+
+$('#show-hide7').toggle();
+var angle7 = 180;
+$('#view-more-btn7').click(function() {
+
+  $(this).css('transform', 'rotate('+ angle7 +'deg)');
+  $('#show-hide7').toggle(300);
+  angle7 = 180 - angle7 ;
+});
+
+
+
+$('#show-hide8').toggle();
+var angle8 = 180;
+$('#view-more-btn8').click(function() {
+
+  $(this).css('transform', 'rotate('+ angle8 +'deg)');
+  $('#show-hide8').toggle(300);
+  angle8 = 180 - angle8 ;
+});
+
+
+
+
+
+
 });
